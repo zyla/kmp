d1b80c38906c5e161a52b9ab8322afa18f901b35
Maciej Bielecki <zyla@prati.pl>
2020-05-05 14:42:50 +0200

    prepare implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
 type PrefixTable = Vec<Option<usize>>;
 
 fn prepare(needle: &[u8]) -> PrefixTable {
-    needle.iter().map(|_| None).collect()
+    let mut prefix_len: Option<usize> = None;
+    let mut prefix_table: PrefixTable = Vec::with_capacity(needle.len());
+
+    // Don't try to match from the beginning
+    prefix_table.push(None);
+
+    for i in 1..needle.len() {
+        // Push prefix_len at index i.
+        prefix_table.push(prefix_len);
+
+        // Now we have prefix_table filled up to (including) i.
+        // We're computing prefix_len for i+1.
+
+        // Index of the next expected character of the current prefix.
+        // If we don't have a prefix yet, we start at 0.
+        let match_index = prefix_len.unwrap_or(0);
+
+        prefix_len = if needle[i] == needle[match_index] {
+            // Extend the current prefix (including an empty prefix).
+            Some(match_index + 1)
+        } else {
+            match prefix_table[match_index] {
+                // Try to extend next prefix
+                Some(len) if needle[i] == needle[len] => Some(len + 1),
+
+                // Otherwise start from the beginning
+                _ if needle[i] == needle[0] => Some(1),
+
+                _ => None,
+            }
+        };
+    }
+    prefix_table
 }
 
 fn search(needle: &[u8], prefix_table: &PrefixTable, haystack: &[u8]) -> Option<usize> {
@@ -31,8 +63,23 @@ fn search(needle: &[u8], prefix_table: &PrefixTable, haystack: &[u8]) -> Option<
     }
 }
 
-#[cfg(test)] mod tests {
+#[cfg(test)]
+mod tests {
     use super::*;
+
+    #[test]
+    fn test_prepare() {
+        assert_eq!(prepare(b"AAA"), vec![None, None, Some(1)]);
+        assert_eq!(prepare(b"ABC"), vec![None, None, None]);
+        assert_eq!(
+            prepare(b"ABABABXYZ"),
+            vec![None, None, None, Some(1), Some(2), Some(3), Some(4), None, None]
+        );
+        assert_eq!(
+            prepare(b"ABCABABC"),
+            vec![None, None, None, None, Some(1), Some(2), Some(1), Some(2)]
+        );
+    }
 
     #[test]
     fn test_search() {
